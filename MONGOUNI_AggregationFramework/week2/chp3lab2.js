44686ff7878f88346850895fbf90d2afbb0ab6fd
Pedro Correia <pedrocorreia.rs@gmail.com>
2018-07-29 17:19:24 -0300

    Add files via upload

---
+++ MONGOUNI_AggregationFramework/week2/chp3lab2.js
@@ -0,0 +1,49 @@
+var pipeline = [
+    {
+        $match: {
+            "languages": "English",
+            "imdb.rating": {"$gte": 0}
+        }
+    },
+    {
+        $unwind: "$cast"
+    },
+    {
+        $project: {
+            "_id": 0,
+            "imdb.rating": 1,
+            "cast": 1,
+            "languages": 1,
+            "title": 1
+        }
+    },
+    {
+        $group: {
+            "_id": "$cast",
+            "numFilms": {"$sum": 1},
+            "average": {"$avg": "$imdb.rating"}
+        }
+    },
+    {
+        $sort: {
+            "numFilms": -1
+        }
+    },
+    {
+        $limit: 1
+    },
+    {
+        $project: {
+            "_id": 1,
+            "numFilms": 1,
+            "average": {
+                "$divide": 
+                    [{
+                        "$trunc": {"$multiply": ["$average", 10]}
+                    }, 
+                    10
+                ]
+            }
+        }
+    }
+]
